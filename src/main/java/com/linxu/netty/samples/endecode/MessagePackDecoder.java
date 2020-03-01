ceba3bc001029610bc69ec3ab9b456ee4a29f64b
linux5396 <929159338@qq.com>
2020-03-02 00:41:20 +0800

    fix 多重编解码器

--- src/main/java/com/linxu/netty/samples/endecode/MessagePackDecoder.java
+++ src/main/java/com/linxu/netty/samples/endecode/MessagePackDecoder.java
@@ -12,15 +12,20 @@ import java.util.List;
  * @date 2020/2/9
  * <tip>take care of yourself.everything is no in vain.</tip>
  * 利用MESSAGE PACK 实现解码
+ *
+ * //TODO MessageToMessageDecoder this is  二次解码
  */
 public class MessagePackDecoder extends MessageToMessageDecoder<ByteBuf> {
     @Override
     protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf buf, List<Object> list) throws Exception {
+        System.err.println("now call decoder,buf refc:"+buf.refCnt());
+        buf.retain();
         final byte[] array;
         final int length = buf.readableBytes();
         array = new byte[length];
         buf.getBytes(buf.readerIndex(), array, 0, length);
         MessagePack messagePack = new MessagePack();
         list.add(messagePack.read(array));
+        buf.release();
     }
 }
